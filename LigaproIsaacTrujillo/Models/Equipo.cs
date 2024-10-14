776883e48d2657e9633368f083b66d4fe0c9bf22
laboratorio <laboratorio@UPOAULA10206>
2024-10-14 08:41:11 -0500

    Agregar archivos de proyecto.

---
+++ LigaproIsaacTrujillo/Models/Equipo.cs
@@ -0,0 +1,12 @@
+﻿namespace LigaproIsaacTrujillo.Models
+{
+    public class Equipo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Ciudad {  get; set; }
+        public int Titulos { get; set; }
+        public bool AceptaExtrangeros { get; set; }
+        public Estadio Estadio { get; set; }
+    }
+}
