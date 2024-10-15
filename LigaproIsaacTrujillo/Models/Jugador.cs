a7a40ce3a484aab1aa0f58dc599600cd44b3738e
Altermanns <isaac.277.trujillo@gmail.com>
2024-10-15 16:42:49 -0500

    Cambios Jugador

---
+++ LigaproIsaacTrujillo/Models/Jugador.cs
@@ -0,0 +1,12 @@
+﻿namespace LigaproIsaacTrujillo.Models
+{
+    public class Jugador
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Titulos  { get; set; }
+        public Equipo Equipo { get; set; }
+
+    }
+
+}
